420ec3fd3bdb70f377d44e97500f165a2b693a0f
Risson Yao <Risson.Yao@ef.com>
2018-08-20 16:11:10 +0800

    clean up code

--- jingdong/pipelines.py
+++ jingdong/pipelines.py
@@ -38,36 +38,3 @@ class JingdongPipeline(object):
         self.db[self.collection_name].insert(dict(item))
         logging.debug("Item added to MongoDB")
         return item
-
-
-
-
-
-
-
-    # def __init__(self):
-    #     self.conn = pymysql.connect(host='127.0.0.1',port=3306,user ='root',passwd='root',db='jingdong',charset='utf8')
-    #     self.cursor = self.conn.cursor()
-
-    # def process_item(self, item, spider):
-    #     try:#有些标题会重复，所以添加异常
-    #         title = item['title']
-    #         comment_count = item['comment_count']  # 评论数
-    #         shop_url = item['shop_url'] # 店铺链接
-    #         price = item['price']
-    #         goods_url = item['goods_url']
-    #         shops_id = item['shops_id']
-    #         goods_id =int(item['goods_id'])
-    #         #sql = 'insert into jingdong_goods(title,comment_count,shop_url,price,goods_url,shops_id) VALUES (%(title)s,%(comment_count)s,%(shop_url)s,%(price)s,%(goods_url)s,%(shops_id)s,)'
-    #         try:
-    #             self.cursor.execute("insert into jingdong_goods(title,comment_count,shop_url,price,goods_url,shops_id,goods_id)values(%s,%s,%s,%s,%s,%s,%s)", (title,comment_count,shop_url,price,goods_url,shops_id,goods_id))
-
-    #             self.conn.commit()
-    #         except Exception as e:
-    #             pass
-
-    #     except Exception as e:
-    #         pass
-
-    # def close_spider(self):
-    #     self.conn.close()
